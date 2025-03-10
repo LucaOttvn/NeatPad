558bebd3a8ee40d8b795d69f675c74a3f4cbdc25
Luca Ottaviano <lucatremila@gmail.com>
2025-03-10 16:55:22 +0100

    WIP iconBtn creation

--- src/components/GeneralModal.tsx
+++ src/components/GeneralModal.tsx
@@ -1,11 +1,17 @@
 import React from "react";
-import './style.scss';
+import "./style.scss";
+import { handleModal } from "@/utils/globalMethods";
 
 interface GeneralModalProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export default function GeneralModal(props: GeneralModalProps) {
-
-  return <div className="generalModal">{props.children}</div>;
+  return (
+    <div className="generalModalBackdrop" onClick={() => {
+      handleModal()
+    }}>
+      <div className="generalModal">{props.children}</div>
+    </div>
+  );
 }
