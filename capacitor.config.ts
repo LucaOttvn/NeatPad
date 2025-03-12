2549ba718cfcead3ef0479fe362d6d22b334e597
Luca Ottaviano <lucatremila@gmail.com>
2025-03-12 12:08:58 +0100

    mobile modal setup + textarea

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'neat-pad',
-  webDir: 'out'
+  webDir: 'out',
+  server: {
+    url: 'http://192.168.3.11:3000',
+    cleartext: true
+  }
 };
 
 export default config;
