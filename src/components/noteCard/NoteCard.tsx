0e65c6a3affc8612996b7290769e6520122d93b5
Luca Ottaviano <lucatremila@gmail.com>
2025-03-24 19:01:09 +0100

    wip cards text color handling

--- src/components/noteCard/NoteCard.tsx
+++ src/components/noteCard/NoteCard.tsx
@@ -1,7 +1,9 @@
-import { ModalsNames, Note } from "@/utils/interfaces";
+import { colors, Folder, ModalsNames, Note } from "@/utils/interfaces";
 import "./noteCard.scss";
 import AnimatedDiv from "../animatedComponents/AnimatedDiv";
 import { handleModal } from "@/utils/globalMethods";
+import { FolderContext } from "@/contexts/foldersContext";
+import { useContext, useEffect, useState } from "react";
 
 interface NoteCardProps {
   note: Note;
@@ -9,24 +11,67 @@ interface NoteCardProps {
 }
 
 export default function NoteCard(props: NoteCardProps) {
+  const [selectedFolder, setSelectedFolder] = useState<Folder | undefined>(
+    undefined
+  );
+  const [textColor, setTextColor] = useState<string | undefined>(undefined);
+
+  const folderContext = useContext(FolderContext);
+
+  useEffect(() => {
+    setSelectedFolder(
+      folderContext?.folders.find((el) => el.id == folderContext.selectedFolder)
+    );
+  }, [folderContext?.selectedFolder]);
+
+  useEffect(() => {
+    if (selectedFolder) {
+      const foundColor = colors.find(
+        (item) => item.color == selectedFolder?.color
+      );
+      setTextColor(foundColor ? foundColor.text : "lightBlack");
+
+      console.log(selectedFolder);
+    }
+  }, [selectedFolder]);
+
   return (
-    <AnimatedDiv
-      className="noteCard"
-    >
-      <div className="cornerRounder1"></div>
-      <div className="cornerRounder2"></div>
+    <AnimatedDiv className="noteCard">
+      <div
+        className="cornerRounder1"
+        style={{
+          background: selectedFolder
+            ? `var(--${selectedFolder?.color})`
+            : "auto",
+        }}
+      ></div>
+      <div
+        className="cornerRounder2"
+        style={{
+          background: selectedFolder
+            ? `var(--${selectedFolder?.color})`
+            : "auto",
+        }}
+      ></div>
       <div className="cornerRounder3"></div>
-      <div className="overflow-hidden" onClick={() => {
-        props.setSelectedNote(props.note.id);
-        handleModal(true, ModalsNames.updateNote);
-      }}>
+      <div
+        className="overflow-hidden"
+        onClick={() => {
+          props.setSelectedNote(props.note.id);
+          handleModal(true, ModalsNames.updateNote);
+        }}
+      >
         {props.note.title ? (
-          <h1>{props.note.title}</h1>
+          <h1 style={{ color: textColor ? `var(--${textColor})` : "auto" }}>
+            {props.note.title}
+          </h1>
         ) : (
           <h1 style={{ color: "var(--Grey)" }}>No title</h1>
         )}
         {props.note.text ? (
-          <span>{props.note.text}</span>
+          <span style={{ color: textColor ? `var(--${textColor})` : "auto" }}>
+            {props.note.text}
+          </span>
         ) : (
           <span style={{ color: "var(--Grey)" }}>No text</span>
         )}
--- src/components/notesOverview/NotesOverview.tsx
+++ src/components/notesOverview/NotesOverview.tsx
@@ -10,24 +10,42 @@ import AnimatedText from "../animatedComponents/AnimatedText";
 import GeneralModalHeader from "../ui/modals/modalsHeaders/GeneralModalHeader";
 import FolderCreator from "../ui/modals/FolderCreator";
 import { NotesContext } from "@/contexts/notesContext";
+import { FolderContext } from "@/contexts/foldersContext";
 
 export default function NotesOverview() {
   const [selectedNote, setSelectedNote] = useState<number | undefined>(
     undefined
   );
 
+  const [notesToShow, setNotesToShow] = useState<Note[]>([]);
+
   const notesContext = useContext(NotesContext);
+  const folderContext = useContext(FolderContext);
 
   useEffect(() => {
     (async () => {
       const fetchedNotes = await getNotes();
       if (fetchedNotes) {
         notesContext?.setNotes(fetchedNotes);
-        notesContext?.setNotesToShow(fetchedNotes);
       }
     })();
   }, []);
 
+  useEffect(() => {
+    // find the notes contained in the currently selected folder
+    const foundSelectedFolderData = folderContext?.folders.find(
+      (el) => el.id == folderContext.selectedFolder
+    );
+
+    // get the array of notes from the ids of the "notes" array in the current folder and push it
+    const notesToShow: Note[] = [];
+    foundSelectedFolderData?.notes.map((noteId) => {
+      let foundNote = notesContext?.notes.find((el) => el.id == noteId);
+      if (foundNote) notesToShow.push(foundNote);
+    });
+
+    setNotesToShow(notesToShow);
+  }, [folderContext?.selectedFolder]);
 
   function updateNoteOnModalClose() {
     const selectedNoteData = notesContext?.notes.find(
@@ -94,11 +112,11 @@ export default function NotesOverview() {
       </GeneralModal>
 
       {/* pinnedSection */}
-      {notesContext?.notesToShow.some((el) => el.pinned) && (
+      {notesContext?.notes.some((el) => el.pinned) && (
         <section className="notesSection">
           <AnimatedText className="title ms-2" text="Pinned" />
           <div className="notes">
-            {notesContext?.notesToShow
+            {notesContext?.notes
               .filter((el) => el.pinned)
               .map((note: Note) => {
                 return (
@@ -114,22 +132,23 @@ export default function NotesOverview() {
       )}
       {/* notesSection */}
       <section className="notesSection">
-        <div className="flex flex-col items-start">
+        {folderContext?.selectedFolder ? <AnimatedText className="title ms-2" text={folderContext.folders.find(el => el.id == folderContext.selectedFolder)?.name || 'No name'} /> : <div className="flex flex-col items-start">
           <AnimatedText className="title ms-2" text="My" />
           <AnimatedText className="title ms-2" text="Notes" />
-        </div>
+        </div>}
         <div className="notes">
-          {notesContext?.notesToShow
-            .filter((el) => el.pinned == false)
-            .map((note: Note) => {
-              return (
-                <NoteCard
-                  key={note.id}
-                  note={note}
-                  setSelectedNote={setSelectedNote}
-                />
-              );
-            })}
+          {(folderContext?.selectedFolder
+            ? notesToShow.filter((el) => el.pinned == false)
+            : notesContext?.notes!
+          ).map((note: Note) => {
+            return (
+              <NoteCard
+                key={note.id}
+                note={note}
+                setSelectedNote={setSelectedNote}
+              />
+            );
+          })}
         </div>
       </section>
     </div>
