e92d450f5c2f71a1cee82fa3817bf87110af30bf
Luca Ottaviano <lucatremila@gmail.com>
2025-05-26 18:53:12 +0200

    delete account implemented

--- src/api/supabaseClient.ts
+++ src/api/supabaseClient.ts
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://wzkmtpvegjxyzocdsttn.supabase.co'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
+const supabaseServiceRoleKey = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY || '';
 export const supabase = createClient(supabaseUrl, supabaseKey!)
+// this is needed wherever the user has to perform actions that require admin permissions
+export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceRoleKey);
+
 
