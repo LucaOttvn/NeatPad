8e11a8493008d172d5981dcbd6b686475ee89345
Luca Ottaviano <lucatremila@gmail.com>
2025-06-01 15:39:56 +0200

    wip signup / signin setup

--- src/components/ui/modals/LoginModal.tsx
+++ src/components/ui/modals/LoginModal.tsx
@@ -1,7 +1,6 @@
-import { signIn, signUp } from '@/api/user';
- 
 import { UserContext } from '@/contexts/userContext';
 import { selectedModal } from '@/utils/signals';
+import { useRouter } from 'next/navigation';
 import React, { useContext, useState } from 'react';
 
 interface LoginModalProps {
@@ -10,8 +9,10 @@ interface LoginModalProps {
 
 export default function LoginModal(props: LoginModalProps) {
 
+    const router = useRouter()
+
     const userContext = useContext(UserContext);
-     
+
     const [formData, setFormData] = useState({ email: "", password: "" });
 
     function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
@@ -28,16 +29,37 @@ export default function LoginModal(props: LoginModalProps) {
         }
     }
 
+
     async function handleSubmit() {
         selectedModal.value = undefined
-        const user = props.creatingAccount
-            ? await signUp(formData.email, formData.password)
-            : await signIn(formData.email, formData.password);
-        if (user) {
-            userContext?.setUser(user.user);
+
+        try {
+            const response = await fetch(`/api/${props.creatingAccount ? 'signup' : 'signin'}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email: formData.email, password: formData.password }),
+            });
+
+            if (response.ok) {
+                const newUser = await response.json()
+
+                console.log(newUser)
+                userContext?.setUser(newUser);
+
+            } else {
+                if (response.status == 409) {
+                    alert('This user already exists')
+                }
+                else {
+                    alert('Something went wrong, retry')
+                }
+            }
+        } catch (error: any) {
+            console.error('Error during signup:', error);
         }
     }
-
 
     return (
         <div className="loginModal w-full h-full center flex-col gap-8">
