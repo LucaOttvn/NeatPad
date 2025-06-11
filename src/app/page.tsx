a29bca9cd225d2122def27d4f1aada996f7d40c5
Luca Ottaviano <lucatremila@gmail.com>
2025-06-11 11:00:03 +0200

    fix inital loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,23 @@
 "use client";;
 import TopBar from "@/components/ui/TopBar";
-import Image from "next/image";
 import { useContext, useEffect, useLayoutEffect } from "react";
 import { ScaleLoader } from "react-spinners";
 import AnimatedDiv from "@/components/animatedComponents/AnimatedDiv";
 import NotesOverview from "@/components/notesOverview/NotesOverview";
-import { ModalsNames, Note, User } from "@/utils/interfaces";
+import { User } from "@/utils/interfaces";
 import { UserContext } from "@/contexts/userContext";
 import GeneralSideMenu from "@/components/ui/SideMenu";
 import Login from "@/components/Login";
 import GeneralModal from "@/components/ui/modals/GeneralModal";
-import { createNote, deleteNote } from "@/db/notes";
-import { NotesContext } from "@/contexts/notesContext";
-import { flushSync } from "react-dom";
-import gsap from 'gsap';
-import { animateDivUnmount } from "@/utils/globalMethods";
 import { useSignals } from "@preact/signals-react/runtime";
-import { isMobile, loading, selectedModal } from "@/utils/signals";
+import { isMobile, loading } from "@/utils/signals";
 import { getUserById } from "@/db/user";
+import NewNoteButton from "@/components/ui/NewNoteButton";
 
 export default function Home() {
   useSignals()
 
   const userContext = useContext(UserContext);
-  const notesContext = useContext(NotesContext);
 
   useLayoutEffect(() => {
     function checkScreenSize() {
@@ -38,10 +32,9 @@ export default function Home() {
     loading.value = true
     const token = localStorage.getItem('JWT');
 
-
-    if (token) {
-      async function verifyToken() {
-        try {
+    async function verifyToken() {
+      try {
+        if (token) {
           const response = await fetch('/api/verifyToken', {
             headers: {
               Authorization: `Bearer ${token}`,
@@ -56,62 +49,20 @@ export default function Home() {
             localStorage.removeItem('JWT');
             userContext?.setUser(undefined);
           }
-        } catch (error) {
-          console.error('Error verifying token:', error);
-          localStorage.removeItem('JWT');
+        } else {
           userContext?.setUser(undefined);
         }
-      };
-
-      verifyToken();
-    } else {
-      userContext?.setUser(undefined);
-    }
-
-    loading.value = false
-  }, []);
-
-
-  // delete mode animation for the add/delete note button
-  useEffect(() => {
-    gsap.to('#newNoteBtn', {
-      rotateY: notesContext?.deleteMode.active ? '180deg' : '0deg',
-      background: notesContext?.deleteMode.active ? 'var(--Red)' : '',
-      duration: 0.2,
-      onComplete: () => {
-        gsap.to('#trashBtn', {
-          scale: notesContext?.deleteMode.active ? 1 : 0,
-          duration: 0.2
-        })
-        gsap.to('#plusBtn', {
-          scale: notesContext?.deleteMode.active ? 0 : 1,
-          duration: 0.2
-        })
+      } catch (error) {
+        console.error('Error verifying token:', error);
+        localStorage.removeItem('JWT');
+        userContext?.setUser(undefined);
+      } finally {
+        loading.value = false
       }
-    })
-  }, [notesContext?.deleteMode.active]);
+    };
 
-  // when the user clicks on the plus button, the createNote() gets triggered, this happens because the NoteEditor component needs a note with an already existing id because it's supposed to edit notes, not creating new ones (as you can guess from the name)
-  async function openNewNoteModal() {
-    const newNote: Note = {
-      user: userContext!.user!.id!,
-      title: '',
-      text: '',
-      last_update: new Date(),
-      pinned: false,
-      folder: null
-    }
-    let newNoteFromDB = await createNote(newNote)
-    if (newNoteFromDB) {
-      if (!notesContext) return null
-      let arr = [...notesContext.notes, newNoteFromDB]
-      flushSync(() => {
-        notesContext.setNotes(arr)
-        notesContext.setSelectedNote(newNoteFromDB.id)
-      })
-      selectedModal.value = ModalsNames.newNote
-    }
-  }
+    verifyToken();
+  }, []);
 
   return (
     <>
@@ -119,59 +70,17 @@ export default function Home() {
         <ScaleLoader color={"white"} loading={true} />
       </div>}
 
-      {userContext?.user ? (
+      {userContext?.user &&
         <AnimatedDiv className="appContainer w-full h-full flex start">
           <TopBar />
           <div className="w-full h-full flex" style={{ marginTop: "8rem" }}>
             <GeneralSideMenu />
             <NotesOverview />
           </div>
-
-          <button
-            id="newNoteBtn"
-            className="addBtn"
-            style={{ borderRadius: "50%" }}
-            onClick={() => {
-              if (notesContext?.deleteMode.active) {
-                let notesToDelete = notesContext.deleteMode.notes.map((noteId) => {
-                  return 'noteCard' + noteId
-                })
-                animateDivUnmount(notesToDelete, () => {
-                  let updatedNotes = notesContext.notes.filter(note => !notesContext.deleteMode.notes.includes(note.id!));
-                  notesContext.setNotes(updatedNotes)
-                  deleteNote(notesContext.deleteMode.notes)
-                  notesContext.setDeleteMode({ active: false, notes: [] })
-                })
-              }
-              else {
-                openNewNoteModal()
-              }
-            }}
-          >
-            <Image
-              id="trashBtn"
-              src="/icons/trash.svg"
-              width={25}
-              height={25}
-              alt=""
-              draggable={false}
-            />
-            <Image
-              id="plusBtn"
-              src="/icons/plus.svg"
-              width={25}
-              height={25}
-              alt=""
-              draggable={false}
-            />
-          </button>
-        </AnimatedDiv>
-      ) : (
-        <Login />
-      )}
+          <NewNoteButton />
+        </AnimatedDiv>}
+      {!userContext?.user && <Login />}
       <GeneralModal />
-
-
     </>
   );
 }
