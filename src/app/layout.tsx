48b2f356d7f50888b452ff55984e8a62d0bcf17b
Luca Ottaviano <lucatremila@gmail.com>
2025-07-01 17:48:24 +0200

    start folders handling update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,10 +28,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta
-          name="viewport"
-          content="viewport-fit=cover, width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
-        />
         <link rel="icon" type="image/png" href="/images/logo.png" /></head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
