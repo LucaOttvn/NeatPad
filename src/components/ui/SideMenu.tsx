9ee2338703ec31f4a5a87988d7aeeda8b405b953
Luca Ottaviano <lucatremila@gmail.com>
2025-07-17 18:00:00 +0000

    upddate sideMenu handling

--- src/components/ui/SideMenu.tsx
+++ src/components/ui/SideMenu.tsx
@@ -1,28 +1,38 @@
 import { Folder } from "@/utils/interfaces";
 import { useEffect, useState } from "react";
 import FolderCard from "../FolderCard";
+import { getFolders } from "@/api/folders";
 
 export default function GeneralSideMenu() {
   const [folders, setFolders] = useState<Folder[]>([]);
 
   useEffect(() => {
-    setFolders([
-      { id: 1, name: "Music", color: "Red" },
-      { id: 2, name: "Groceries", color: "Yellow" },
-      { id: 3, name: "Shopping", color: "Green" },
-      { id: 4, name: "Gaming", color: "Blue" },
-      { id: 5, name: "Todo", color: "Purple" },
-      { id: 6, name: "Reminders", color: "Brown" },
-      { id: 7, name: "Gifts", color: "Orange" },
-    ]);
+    (async () => {
+      const fetchedfolders = await getFolders();
+      if (fetchedfolders) setFolders(fetchedfolders);
+    })();
   }, []);
+
+  // useEffect(() => {
+  //   setFolders([
+  //     { id: 1, name: "Music", color: "Red", notes: [] },
+  //     { id: 2, name: "Groceries", color: "Yellow", notes: [] },
+  //     { id: 3, name: "Shopping", color: "Green", notes: [] },
+  //     { id: 4, name: "Gaming", color: "Blue", notes: [] },
+  //     { id: 5, name: "Todo", color: "Purple", notes: [] },
+  //     { id: 6, name: "Reminders", color: "Brown", notes: [] },
+  //     { id: 7, name: "Gifts", color: "Orange", notes: [] },
+  //     { id: 8, name: "Gifts", notes: [] },
+  //   ]);
+  // }, []);
   return (
     <div className="generalSideMenu">
-      <div className="m-8 mt-10 flex flex-col gap-8">
+      <div className="h-full m-8 mt-10 flex flex-col gap-8">
         <div className="flex flex-col items-start">
           <span className="title ms-2">My</span>
           <span className="title ms-2">Folders</span>
         </div>
+        <button className="mainBtn mb-5">Add folder</button>
 
         <div className="flex flex-col">
           {folders.map((folder) => {
