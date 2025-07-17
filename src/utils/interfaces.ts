85facab3d449924a53c80d900f00815ae57427c6
Luca Ottaviano <lucatremila@gmail.com>
2025-07-17 18:00:00 +0000

    update modals sizes handling

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -16,6 +16,46 @@ export enum ModalsNames {
     folderHandler = "folderHandlerModal",
     settings = "settingsModal",
 }
+
+interface Modal {
+    name: string
+    width: string
+    height: string
+}
+
+export const modalsList: Modal[] = [
+    {
+        name: ModalsNames.newNote,
+        width: '80%',
+        height: '80%'
+    },
+    {
+        name: ModalsNames.updateNote,
+        width: '80%',
+        height: '80%'
+    },
+    {
+        name: ModalsNames.login,
+        width: '30%',
+        height: '50%'
+    },
+    {
+        name: ModalsNames.createAccount,
+        width: '30%',
+        height: '60%'
+    },
+    {
+        name: ModalsNames.folderHandler,
+        width: '80%',
+        height: '80%'
+    },
+    {
+        name: ModalsNames.settings,
+        width: '60%',
+        height: '90%'
+    },
+]
+
 export enum SideMenusNames {
     folders = "foldersSideMenu",
 }
