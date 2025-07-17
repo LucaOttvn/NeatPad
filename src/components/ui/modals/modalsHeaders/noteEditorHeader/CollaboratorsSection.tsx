02ac624b61121c5d75611a2356343eea07eee5e4
Luca Ottaviano <lucatremila@gmail.com>
2025-07-17 18:00:00 +0000

    collaborators section added

---
+++ src/components/ui/modals/modalsHeaders/noteEditorHeader/CollaboratorsSection.tsx
@@ -0,0 +1,33 @@
+import React, { Dispatch, SetStateAction, useEffect } from 'react';
+import gsap from 'gsap'
+import './noteEditorHeader.scss';
+
+interface CollaboratorsSectionProps {
+    collaboratorsSectionOpen: boolean
+}
+
+export default function CollaboratorsSection(props: CollaboratorsSectionProps) {
+
+    useEffect(() => {
+        gsap.to(".collaboratorsSection", {
+            height: props.collaboratorsSectionOpen ? "auto" : 0,
+            marginBottom: props.collaboratorsSectionOpen ? '1rem' : 0,
+            duration: 0.2,
+            ease: 'power4.out',
+        });
+    }, [props.collaboratorsSectionOpen])
+
+    return (
+        <section className="collaboratorsSection">
+            <span className="font-bold mx-5 mt-5">Collaborators</span>
+            <div className="collaboratorsList">
+                {['test1@gmail.com', 'test2@gmail.com', 'test3@gmail.com', 'test4@gmail.com', 'test5@gmail.com', 'test6@gmail.com'].map((email) => {
+                    return <div key={email} className="emailBox">
+                        <span>{email}</span>
+                    </div>
+                })}
+            </div>
+            <div className="footer">ciao</div>
+        </section>
+    );
+}
