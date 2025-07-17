d6fb2d0522da7d24d81f535871004a2f288cfd5a
Luca Ottaviano <lucatremila@gmail.com>
2025-07-17 18:00:00 +0000

    animations updated

--- src/components/animatedComponents/AnimatedDiv.tsx
+++ src/components/animatedComponents/AnimatedDiv.tsx
@@ -1,5 +1,4 @@
-import { CSSProperties, ReactNode, useEffect } from "react";
-import { gsap, Power4 } from "gsap";
+import { CSSProperties, ReactNode } from "react";
 import "./animatedStyle.scss";
 
 interface AnimatedDivProps {
@@ -15,14 +14,7 @@ interface AnimatedDivProps {
 }
 
 export default function AnimatedDiv(props: AnimatedDivProps) {
-  useEffect(() => {
-    gsap.to(".animatedDiv", {
-      opacity: 1,
-      ease: Power4.easeOut,
-      duration: 1,
-    });
-  }, []);
-
+  
   return (
     <div
       id={props.id || ''}
