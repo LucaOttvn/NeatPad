ebd9d177951316d8b15b10813b3be63d76bfc7d8
Luca Ottaviano <lucatremila@gmail.com>
2025-07-17 18:00:00 +0000

    wip RecoverPasswordPage

---
+++ src/app/recover-password/page.tsx
@@ -0,0 +1,9 @@
+import ResetPasswordForm from "@/components/ui/ResetPasswordForm";
+
+export default function RecoverPasswordPage() {
+  return (
+    <div className="w-full h-full">
+        <ResetPasswordForm/>
+    </div>
+  );
+}
