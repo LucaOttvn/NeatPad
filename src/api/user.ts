ff19d1f237b8cd301f3457318d14591a94d08333
Luca Ottaviano <lucatremila@gmail.com>
2025-07-17 18:00:00 +0000

    update delete account

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,59 +1,17 @@
-import { User } from "@/utils/interfaces";
 import { supabase } from "./supabaseClient";
 
+export async function deleteUser(id: number) {
+    const { data, error } = await supabase
+        .from("users")
+        .delete()
+        .eq("id", id)
+        .select()
+        .single();
 
-
-
-// export async function getUser(): Promise<User | undefined> {
-//     const { data, error } = await supabase.auth.getUser()
-//     if (error) return undefined
-//     return data.user
-// }
-
-export async function newSignIn(email: string, password: string) {
-    email = 'testEnail'
-    password = 'testPswd'
-}
-
-export async function signIn(email: string, password: string) {
-    const { data, error } = await supabase.auth.signInWithPassword({
-        email: email,
-        password: password,
-    })
     if (error) {
-        console.error("Error fetching data:", error);
+        console.error("Error deleting user:", error);
         return null;
     }
 
     return data;
-}
-
-export async function signUp(email: string, password: string) {
-    const { data, error } = await supabase.auth.signUp({
-        email: email,
-        password: password,
-    })
-    if (error) {
-        console.error("Error fetching data:", error);
-        return null;
-    }
-
-    return data;
-}
-
-interface UpdateAuthUserParams {
-    email?: string;
-    password?: string;
-}
-
-export async function updateUser(updatedUserInfo: UpdateAuthUserParams) {
-
-    const { data, error } = await supabase.auth.updateUser(updatedUserInfo);
-
-    if (error) {
-        console.error("Error updating user:", error);
-        return null;
-    }
-
-    return data?.user || null;
 }
