4d18a4d4ced39488db042c6ca4b875ccaf8d6b84
Luca Ottaviano <lucatremila@gmail.com>
2025-07-17 18:00:00 +0000

    delete account implemented

--- src/components/ui/modals/settings/SettingsModal.tsx
+++ src/components/ui/modals/settings/SettingsModal.tsx
@@ -4,7 +4,8 @@ import { useContext } from 'react';
 import './settingsModal.scss';
 import { ReactSVG } from 'react-svg';
 import ResetPasswordForm from '../../ResetPasswordForm';
-import { deleteSupabaseUser } from '@/serverActions/authActions';
+import { deleteUserAccount } from '@/serverActions/authActions';
+
 
 interface SettingsModalProps { }
 
@@ -21,7 +22,7 @@ export default function SettingsModal(props: SettingsModalProps) {
   function handleDeleteSupabaseUser() {
     if (confirm('Do you really want to delete this account?')) {
       if (userContext?.user) {
-        deleteSupabaseUser(userContext.user.id)
+        deleteUserAccount(userContext.user.id)
         logout()
       }
     }
--- src/serverActions/authActions.ts
+++ src/serverActions/authActions.ts
@@ -1,17 +1,22 @@
-'use server'
+'use server';
+import { supabaseAdmin } from '@/api/supabaseClient';
+import { revalidatePath } from 'next/cache';
+import { cookies } from 'next/headers';
 
-import { supabase } from "@/api/supabaseClient";
 
-export async function deleteSupabaseUser(userId: string) {
-    try {
-        const { error } = await supabase.auth.admin.deleteUser(userId);
-        if (error) {
-            console.error('Error deleting user:', error);
-            return { error: error.message };
-        }
-        return { data: `User ${userId} deleted successfully` };
-    } catch (error) {
-        console.error('Server error during delete:', error);
-        return { error: 'Internal Server Error' };
+export async function deleteUserAccount(userId: string) {
+  try {
+    const { error } = await supabaseAdmin.auth.admin.deleteUser(userId);
+    if (error) {
+      console.error('Error deleting user:', error);
+      return { error: error.message };
     }
+    console.log(`User ${userId} deleted successfully`);
+    (await cookies()).delete('supabase-auth-token');
+    revalidatePath('/');
+    return { success: true };
+  } catch (error) {
+    console.error('Server error during delete:', error);
+    return { error: 'Internal Server Error' };
+  }
 }
