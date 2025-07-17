d2d3b255cb81a45dcbe296da033bc9d136dde66d
Luca Ottaviano <lucatremila@gmail.com>
2025-07-17 18:00:00 +0000

    delete mode handling complete

--- src/components/noteCard/NoteCard.tsx
+++ src/components/noteCard/NoteCard.tsx
@@ -1,11 +1,11 @@
 import { colors, ModalsNames, Note } from "@/utils/interfaces";
 import "./noteCard.scss";
 import AnimatedDiv from "../animatedComponents/AnimatedDiv";
-import { handleModal } from "@/utils/globalMethods";
 import { FoldersContext } from "@/contexts/foldersContext";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useRef } from "react";
 import { NotesContext } from "@/contexts/notesContext";
 import { ModalsContext } from "@/contexts/modalsContext";
+import gsap from 'gsap';
 
 interface NoteCardProps {
   note: Note;
@@ -15,6 +15,7 @@ export default function NoteCard(props: NoteCardProps) {
   const foldersContext = useContext(FoldersContext);
   const notesContext = useContext(NotesContext);
   const modalsContext = useContext(ModalsContext);
+  const timerRef = useRef<any>(null);
 
   const foundParentFolder = foldersContext?.folders.find((el) =>
     props.note.id ? el.notes.includes(props.note.id) : undefined
@@ -24,13 +25,67 @@ export default function NoteCard(props: NoteCardProps) {
     colors.find((item) => item.color == foundParentFolder?.color)?.text ||
     "White";
 
+  useEffect(() => {
+    return () => {
+      clearTimeout(timerRef.current);
+    };
+  }, []);
+
+  useEffect(() => {
+    let noteToDelete = notesContext?.deleteMode.notes.find(noteId => noteId == props.note.id)
+
+    gsap.to('#noteCard' + props.note.id, {
+      border: noteToDelete ? 'solid 4px var(--Red)' : 'none',
+      duration: 0.2
+    })
+  }, [notesContext?.deleteMode]);
+
+
+  function handleTouchStart() {
+
+    // if in delete mode
+    if (notesContext!.deleteMode.active) {
+      let notesToDeleteUpdated: number[] = []
+
+      // is the cliked card already selected?
+      let isNoteAlreadySelected = notesContext?.deleteMode.notes.some(noteId => noteId == props.note.id)
+
+      // if it's already selected remove it, otherwise add it
+      notesToDeleteUpdated = isNoteAlreadySelected ? notesContext!.deleteMode.notes.filter(noteId => noteId != props.note.id!) : [...notesContext!.deleteMode.notes, props.note.id!]
+
+      notesContext?.setDeleteMode({active: true, notes: notesToDeleteUpdated})
+    }
+    else {
+      // start the timer to detect long presses
+      timerRef.current = setTimeout(() => {
+        let notesToDeleteUpdated: number[] = [...notesContext!.deleteMode.notes, props.note.id!]
+        notesContext?.setDeleteMode({active: true, notes: notesToDeleteUpdated})
+      }, 500);
+    }
+  }
+
+  // if the user removes the finger from the screen before the end of the timeout, clear the timer so no card selection animation gets triggered
+  function handleTouchEnd() {
+    clearTimeout(timerRef.current);
+    // if delete mode is off
+    if (!notesContext!.deleteMode.active) {
+      notesContext?.setSelectedNote(props.note.id);
+      modalsContext?.setSelectedModal(ModalsNames.updateNote)
+    }
+    // this is the case described in notesContext.tsx about the deleteMode handling
+    if (notesContext?.deleteMode.active && notesContext.deleteMode.notes.length == 0) {
+      notesContext.setDeleteMode({active: false, notes: notesContext.deleteMode.notes})
+    }
+  }
+
   return (
     <AnimatedDiv
+      id={'noteCard' + props.note.id}
       className="noteCard"
-      onClick={() => {
-        notesContext?.setSelectedNote(props.note.id);
-        modalsContext?.setSelectedModal(ModalsNames.updateNote)
-      }}
+      // when the user long press but then drags the finger outside of the div don't trigger the long press event
+      onTouchMove={handleTouchEnd}
+      onTouchStart={handleTouchStart}
+      onTouchEnd={handleTouchEnd}
     >
       <div
         className="cornerRounder1"
