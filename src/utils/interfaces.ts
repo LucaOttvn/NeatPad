9783fd542db4c99649635b3f2e384bd94bba07fe
Luca Ottaviano <lucatremila@gmail.com>
2025-07-17 18:00:00 +0000

    update folderHandler ui + scroll below modal prevention

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -52,8 +52,8 @@ export const modalsList: Modal[] = [
     },
     {
         name: ModalsNames.folderHandler,
-        width: '50%',
-        height: '60%'
+        width: '70%',
+        height: '70%'
     },
     {
         name: ModalsNames.settings,
