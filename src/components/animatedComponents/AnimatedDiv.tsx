9b6227fc6463545bb9d32caec14e5129ea9aee40
Luca Ottaviano <lucatremila@gmail.com>
2025-07-17 18:00:00 +0000

    fix notes colors

--- src/components/animatedComponents/AnimatedDiv.tsx
+++ src/components/animatedComponents/AnimatedDiv.tsx
@@ -5,7 +5,7 @@ import "./animatedStyle.scss";
 interface AnimatedDivProps {
   children: ReactNode;
   className?: string;
-  onClick?: ()=> void
+  onClick?: () => void;
 }
 
 export default function AnimatedDiv(props: AnimatedDivProps) {
@@ -13,11 +13,25 @@ export default function AnimatedDiv(props: AnimatedDivProps) {
     gsap.to(".animatedDiv", {
       opacity: 1,
       ease: Power4.easeOut,
-      duration: 1
+      duration: 1,
     });
+    return () => {
+      // gsap.to(".animatedDiv", {
+      //   opacity: 0,
+      //   ease: Power4.easeOut,
+      //   duration: 1,
+      // });
+    };
   }, []);
 
-  return <div className={`animatedDiv ${props.className || ''}`} onClick={()=>{
-    if (props.onClick) props.onClick()
-  }}>{props.children}</div>;
+  return (
+    <div
+      className={`animatedDiv ${props.className || ""}`}
+      onClick={() => {
+        if (props.onClick) props.onClick();
+      }}
+    >
+      {props.children}
+    </div>
+  );
 }
