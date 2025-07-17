689f0969b34b3bf7a9192ec3336c083304769189
Luca Ottaviano <lucatremila@gmail.com>
2025-07-17 18:00:00 +0000

    delete account added

--- src/components/ui/modals/settings/SettingsModal.tsx
+++ src/components/ui/modals/settings/SettingsModal.tsx
@@ -4,6 +4,7 @@ import { useContext } from 'react';
 import './settingsModal.scss';
 import { ReactSVG } from 'react-svg';
 import ResetPasswordForm from '../../ResetPasswordForm';
+import { deleteSupabaseUser } from '@/serverActions/authActions';
 
 interface SettingsModalProps { }
 
@@ -17,17 +18,26 @@ export default function SettingsModal(props: SettingsModalProps) {
     modalsContext?.setSelectedModal(undefined)
   }
 
+  function handleDeleteSupabaseUser() {
+    if (confirm('Do you really want to delete this account?')) {
+      if (userContext?.user) {
+        deleteSupabaseUser(userContext.user.id)
+        logout()
+      }
+    }
+  }
+
   return (
     <div className='settingsModal'>
       <span className='title'>Settings</span>
       <div className='w-full h-full flex flex-col justify-between mt-20'>
         <section className='profileSection'>
           <h1 className='w-full center' style={{ fontSize: '140%' }}>Change password</h1>
-          <ResetPasswordForm/>
+          <ResetPasswordForm />
         </section>
         <div className='flex flex-col items-center gap-5'>
           <button className='mainBtn center gap-2' style={{ background: 'var(--darkGrey)' }} onClick={logout}><ReactSVG src={'/icons/logout.svg'} className="icon" />Logout</button>
-          <button className='mainBtn' style={{ background: 'var(--Red)' }} >Delete account</button>
+          <button className='mainBtn' style={{ background: 'var(--Red)' }} onClick={() => { handleDeleteSupabaseUser() }}>Delete account</button>
         </div>
       </div>
     </div>
