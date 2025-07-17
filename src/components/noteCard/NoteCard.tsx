7f7e1b8656e7aa6c163d444bcce42f2e1b3f52ea
Luca Ottaviano <lucatremila@gmail.com>
2025-07-17 18:00:00 +0000

    fix scroll bug

--- src/components/noteCard/NoteCard.tsx
+++ src/components/noteCard/NoteCard.tsx
@@ -38,28 +38,21 @@ export default function NoteCard(props: NoteCardProps) {
       outline: noteToDelete ? 'solid 4px var(--Red)' : 'none',
       duration: 0.2
     })
+
+    // this is the case described in notesContext.tsx about the deleteMode handling
+    if (notesContext?.deleteMode.active && notesContext.deleteMode.notes.length == 0) {
+      notesContext.setDeleteMode({ active: false, notes: notesContext.deleteMode.notes })
+    }
   }, [notesContext?.deleteMode]);
 
 
   function handleTouchStart() {
 
-    // if in delete mode
-    if (notesContext!.deleteMode.active) {
-      let notesToDeleteUpdated: number[] = []
-
-      // is the cliked card already selected?
-      let isNoteAlreadySelected = notesContext?.deleteMode.notes.some(noteId => noteId == props.note.id)
-
-      // if it's already selected remove it, otherwise add it
-      notesToDeleteUpdated = isNoteAlreadySelected ? notesContext!.deleteMode.notes.filter(noteId => noteId != props.note.id!) : [...notesContext!.deleteMode.notes, props.note.id!]
-
-      notesContext?.setDeleteMode({active: true, notes: notesToDeleteUpdated})
-    }
-    else {
+    if (!notesContext?.deleteMode.active) {
       // start the timer to detect long presses
       timerRef.current = setTimeout(() => {
         let notesToDeleteUpdated: number[] = [...notesContext!.deleteMode.notes, props.note.id!]
-        notesContext?.setDeleteMode({active: true, notes: notesToDeleteUpdated})
+        notesContext?.setDeleteMode({ active: true, notes: notesToDeleteUpdated })
       }, 500);
     }
   }
@@ -67,15 +60,6 @@ export default function NoteCard(props: NoteCardProps) {
   // if the user removes the finger from the screen before the end of the timeout, clear the timer so no card selection animation gets triggered
   function handleTouchEnd() {
     clearTimeout(timerRef.current);
-    // if delete mode is off
-    if (!notesContext!.deleteMode.active) {
-      notesContext?.setSelectedNote(props.note.id);
-      modalsContext?.setSelectedModal(ModalsNames.updateNote)
-    }
-    // this is the case described in notesContext.tsx about the deleteMode handling
-    if (notesContext?.deleteMode.active && notesContext.deleteMode.notes.length == 0) {
-      notesContext.setDeleteMode({active: false, notes: notesContext.deleteMode.notes})
-    }
   }
 
   return (
@@ -86,6 +70,27 @@ export default function NoteCard(props: NoteCardProps) {
       onTouchMove={handleTouchEnd}
       onTouchStart={handleTouchStart}
       onTouchEnd={handleTouchEnd}
+      onClick={() => {
+        // if in delete mode
+        if (notesContext!.deleteMode.active) {
+          let notesToDeleteUpdated: number[] = []
+
+          // is the cliked card already selected?
+          let isNoteAlreadySelected = notesContext?.deleteMode.notes.some(noteId => noteId == props.note.id)
+
+          // if it's already selected remove it, otherwise add it
+          notesToDeleteUpdated = isNoteAlreadySelected ? notesContext!.deleteMode.notes.filter(noteId => noteId != props.note.id!) : [...notesContext!.deleteMode.notes, props.note.id!]
+
+          notesContext?.setDeleteMode({ active: true, notes: notesToDeleteUpdated })
+        }
+        else {
+          notesContext?.setSelectedNote(props.note.id);
+          modalsContext?.setSelectedModal(ModalsNames.updateNote)
+        }
+
+
+
+      }}
     >
       <div
         className="cornerRounder1"
