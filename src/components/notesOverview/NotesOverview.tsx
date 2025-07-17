9ede6fe1a5561024ea4e736726c5258fb9700bb2
Luca Ottaviano <lucatremila@gmail.com>
2025-07-17 18:00:00 +0000

    refresh button added

--- src/components/notesOverview/NotesOverview.tsx
+++ src/components/notesOverview/NotesOverview.tsx
@@ -8,6 +8,9 @@ import NotesSection from "../ui/NotesSection";
 import { getNotes } from "@/api/notes";
 import { getFolders } from "@/api/folders";
 import { UserContext } from "@/contexts/userContext";
+import { ReactSVG } from "react-svg";
+import AnimatedDiv from "../animatedComponents/AnimatedDiv";
+import Loader from "../ui/Loader";
 
 export default function NotesOverview() {
   const notesContext = useContext(NotesContext);
@@ -15,6 +18,7 @@ export default function NotesOverview() {
   const userContext = useContext(UserContext);
 
   const [notesToShow, setNotesToShow] = useState<Note[]>([])
+  const [loading, setLoading] = useState(false)
 
   // if there's a selected folder set it
   const foundSelectedFolderData = foldersContext?.selectedFolder ? foldersContext?.folders.find(
@@ -22,23 +26,6 @@ export default function NotesOverview() {
   ) : undefined;
 
   useEffect(() => {
-    // get notes and folders
-    async function fetchData() {
-      try {
-        const [notes, folders] = await Promise.all([getNotes(userContext?.user!.id!), getFolders()]);
-
-        if (notes) {
-          setNotesToShow(notes);
-          notesContext?.setNotes(notes);
-        }
-        if (folders) {
-          foldersContext?.setFolders(folders);
-        }
-      } catch (err) {
-        console.error("Error fetching initial data", err);
-      }
-    }
-
     fetchData();
   }, []);
 
@@ -54,12 +41,49 @@ export default function NotesOverview() {
     }
   }, [foldersContext?.selectedFolder, notesContext?.notes]);
 
+  // get notes and folders
+  async function fetchData() {
+    setLoading(true)
+    setTimeout(async () => {
+      try {
+        const [notes, folders] = await Promise.all([getNotes(userContext?.user!.id!), getFolders()]);
+
+        if (notes) {
+          setNotesToShow(notes);
+          notesContext?.setNotes(notes);
+        }
+        if (folders) {
+          foldersContext?.setFolders(folders);
+        }
+      } catch (err) {
+        console.error("Error fetching initial data", err);
+      }
+      setLoading(false)
+    }, 2000);
+  }
+
+  useEffect(() => {
+    console.log(loading)
+  }, [loading]);
+
   return (
     <div className="notesOverviewContainer">
-      {foldersContext?.selectedFolder ? <AnimatedText className="title" text={foundSelectedFolderData?.name.toUpperCase()!} color={foundSelectedFolderData?.color} /> : <div className="flex flex-col items-start">
-        <AnimatedText className="title" text="My" />
-        <AnimatedText className="title ms-5" text="Notes" />
-      </div>}
+
+      {loading && <Loader />}
+
+      <div className="w-full flex pe-3">
+        {foldersContext?.selectedFolder ? <AnimatedText className="title" text={foundSelectedFolderData?.name.toUpperCase()!} color={foundSelectedFolderData?.color} /> : <div className="flex flex-col items-start">
+          <AnimatedText className="title" text="My" />
+          <AnimatedText className="title ms-5" text="Notes" />
+        </div>}
+        <div className="w-full end">
+          <AnimatedDiv>
+            <button className="mainBtn w-full end gap-1" style={{ padding: '0.7rem' }} onClick={() => {fetchData()}}>
+              <ReactSVG src={`/icons/refresh.svg`} className="icon" style={{ scale: 1.2 }} />
+            </button>
+          </AnimatedDiv>
+        </div>
+      </div>
       {/* pinnedSection (show it only if there's at least one pinned note) */}
       {notesToShow.some((el) => el.pinned) && (
         <NotesSection
@@ -74,7 +98,8 @@ export default function NotesOverview() {
         title="Others"
       /> : <div className="w-full h-full center">
         You haven't saved any notes yet
-        </div>}
+      </div>}
+
     </div>
   );
 }
--- src/components/ui/modals/settings/SettingsModal.tsx
+++ src/components/ui/modals/settings/SettingsModal.tsx
@@ -1,11 +1,8 @@
 import { ModalsContext } from '@/contexts/modalsContext';
 import { UserContext } from '@/contexts/userContext';
-import { useContext, useEffect, useRef, useState } from 'react';
+import { useContext } from 'react';
 import './settingsModal.scss';
 import { ReactSVG } from 'react-svg';
-import { gsap, Power4 } from 'gsap';
-import { updateUser } from '@/api/user';
-import { supabase } from '@/api/supabaseClient';
 import ResetPasswordForm from '../../ResetPasswordForm';
 
 interface SettingsModalProps { }
@@ -13,45 +10,12 @@ interface SettingsModalProps { }
 export default function SettingsModal(props: SettingsModalProps) {
   const userContext = useContext(UserContext)
   const modalsContext = useContext(ModalsContext)
-  const emailInputRef = useRef<HTMLInputElement>(null)
-  const passwordInputRef = useRef<HTMLInputElement>(null)
-  const newPasswordInputRef = useRef<HTMLInputElement>(null)
-
-  const [profileData, setProfileData] = useState({
-    email: { value: userContext?.user?.email, disabled: true },
-    password: { value: '', disabled: true },
-    newPassword: { value: '', disabled: true }
-  })
-
- 
-
-  useEffect(() => {
-    if (!profileData.email.disabled) {
-      emailInputRef.current?.focus();
-    } else {
-      if (userContext?.user?.email != profileData.email.value) {
-        updateUser({ email: 'test3@gmail.com' })
-      }
-    }
-  }, [profileData.email.disabled]);
-
-  
 
   function logout() {
     localStorage.clear();
     userContext?.setUser(null);
     modalsContext?.setSelectedModal(undefined)
   }
-
-  useEffect(() => {
-    if (!profileData.password.disabled) passwordInputRef.current?.focus();
-
-    gsap.to('.newPassword', {
-      height: profileData.password.disabled ? 0 : 'auto',
-      ease: Power4.easeOut,
-      duration: 0.2
-    })
-  }, [profileData.password.disabled]);
 
   return (
     <div className='settingsModal'>
