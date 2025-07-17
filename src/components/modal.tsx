c651b8b5e97089444a3e75e90a11b5ca514d4469
Luca Ottaviano <lucatremila@gmail.com>
2025-07-17 18:00:00 +0000

    First Commit

---
+++ src/components/modal.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.scss'
+import Image from 'next/image';
+
+interface Modal {}
+
+export default function Modal(props: Modal) {
+  return (
+   <></>
+  );
+}
