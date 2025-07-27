29a8b0de22cef964558e9d2a8cad9d220e2c55ed
Luca Ottaviano <lucatremila@gmail.com>
2025-07-27 23:59:56 +0200

    fix overflow x

--- src/utils/globalMethods.tsx
+++ src/utils/globalMethods.tsx
@@ -38,12 +38,10 @@ export function handleModal(
   });
 }
 
-
-
 export function handleSideMenu(target: string | undefined, isMobile: boolean) {
   gsap.to((target ? "#" : '.') + (target || 'generalSideMenu'), {
     width: target ? (isMobile ? '100%' : '35vw') : 0,
-    duration: 0.2,
+    duration: 0.3,
     ease: 'power4.out'
   });
 }
