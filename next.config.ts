20e4a785769f79963ff9f7bab333060558735de9
Luca Ottaviano <lucatremila@gmail.com>
2025-07-28 01:29:38 +0200

    pwa test

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
 
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development', // odisables PWA in development mode, useful for debugging without service worker interference
+  register: true, // registers the service worker for you. Set to false if you want to handle registration manually
+  skipWaiting: true, // forces the new service worker to activate immediately upon update
+});
+
 const nextConfig: NextConfig = {
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "NeatPad",
   description: "",
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
